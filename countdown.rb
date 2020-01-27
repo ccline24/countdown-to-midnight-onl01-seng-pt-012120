a9da8f8c85d90c2cdcd818a760196ff41dc94ff9
Chance cline <clinechance@gmail.com>
2020-01-27 19:35:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,16 +2,16 @@
 
 def countdown(secs)
   until secs == 0 
-  puts "#{secs}SECOND(S)! " 
+  puts "#{secs} SECOND(S)! " 
   secs -=1
 end
 "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(secs)
-  sleep(5)
   while secs > 0 
-  puts  "#{secs}SECOND(S) " 
+  puts  "#{secs} SECOND(S)! "
+  sleep(5)
   secs -= 1 
 end
 "HAPPY NEW YEAR!"
